20586882170c669701e274407f98f5ab54fc9e6d
davidroman <david.roman.grijalbo@gmail.com>
2025-12-03 13:15:53 +0100

    basic program for walking and stop using 2 buttons

--- ros2_ws/src/logitech-controller/logitech-controller/src/controller.cpp
+++ ros2_ws/src/logitech-controller/logitech-controller/src/controller.cpp
@@ -10,13 +10,74 @@
 #include "unitree_go/msg/sport_mode_state.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
+enum action = {
+    MOVE
+    STOP_MOVE
+}
 
 
+float vx = 0;
+float vz = 0;
+
+/*
+  MOVE:
+  nlohmann::json js;
+  js["x"] = vx;
+  js["y"] = vy;
+  js["z"] = vyaw;
+*/
+void JoyCallback(sensor_msgs::msg::Joy::SharedPtr msg){
+     
+    if (msg->buttons[3] == 1) {
+        test_mode_ = MOVE;       
+        RCLCPP_INFO(this->get_logger(), "Mode: MOVE");
+    }
+
+   
+    else if (msg->buttons[0] == 1) {
+        test_mode_ = STOP_MOVE;  
+        RCLCPP_INFO(this->get_logger(), "Mode: STOP_MOVE");
+    }
+    else{
+        test_mode_ = STOP_MOVE;
+        RCLCPP_INFO(this->get_logger(), "No buttons detected");
+    }
+}
+
+void RobotControl(){
+    switch(action){
+        case MOVE:
+            sport_client.Move(req,vx,0,vz);
+            break;
+        case STOP_MOVE:
+            sport_client.StopMove(req);
+            break;
+        default:
+            sport_client.StopMove(req);
+    }
+
+}
+
+
+
+unitree_api::msg::Request req // Unitree Go2 ROS2 request message
 
 int main(int argc, char* argv[]){
     rclcpp::init(argc,argv)
-    auto node = rclcpp::Node::make_shared("controller")
-    auto subscriber = node -> create_publisher<sensor_msgs::msg::Joy>("joy",10,JoystickHandler)
+    auto node = rclcpp::Node::make_shared("controller_ros2_sport_client")
+
+     // Servicio (cliente sport)
+    sport_client = std::make_shared<SportClient>(node.get()); // el servicio espera:  SportClient::SportClient(rclcpp::Node *node)
+
+    auto subscriber = node -> create_publisher<sensor_msgs::msg::Joy>("joy",10,JoyCallback);
+
+    while(rclcpp::ok()){
+        RobotControl();
+        rclcpp::spin_some(node)
+    }
+    rclcpp::shutdown();
+    return 0;
+    
 
 
 
